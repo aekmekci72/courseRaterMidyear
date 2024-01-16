f8e002a6a05c4ab594d91fadcf13a0d044b4e999
shreejalearn <shreeja.das.16@gmail.com>
2024-01-16 14:11:17 -0500

    working on google auth

---
+++ my-app/src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+import { SvelteKitAuth } from "@auth/sveltekit" 
+import {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} from "courseRaterMidyear\my-app\.env" 
+import Google from "@auth/core/providers/google"; 
+
+export const handle = SvelteKitAuth({
+    providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })]
+  });
+  
