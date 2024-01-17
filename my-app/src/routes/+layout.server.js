0888bfbd24f4e8448ad947bfd8940340c8061ead
shreejalearn <shreeja.das.16@gmail.com>
2024-01-17 13:58:43 -0500

    working!!

--- my-app/src/routes/+layout.server.js
+++ my-app/src/routes/+layout.server.js
@@ -1,19 +1,19 @@
-/**
- * @typedef {Object} LoadResult
- * @property {any} session - Description of the session property.
- */
+// /**
+//  * @typedef {Object} LoadResult
+//  * @property {any} session - Description of the session property.
+//  */
 
-/**
- * @function
- * @async
- * @param {Object} event - Description of the event parameter.
- * @returns {Promise<LoadResult>}
- */
-export const load = async (event) => {
-    // Assuming 'event' has a property 'locals' with 'getSession' method
-    const locals = (event && event.locals) || {};
-    return {
-      session: await locals.getSession(),
-    };
-  };
+// /**
+//  * @function
+//  * @async
+//  * @param {Object} event - Description of the event parameter.
+//  * @returns {Promise<LoadResult>}
+//  */
+// export const load = async (event) => {
+//     // Assuming 'event' has a property 'locals' with 'getSession' method
+//     const locals = (event && event.locals) || {};
+//     return {
+//       session: await locals.getSession(),
+//     };
+//   };
   
--- my-app/src/routes/login/+page.svelte
+++ my-app/src/routes/login/+page.svelte
@@ -1,4 +1,4 @@
-import { SvelteKitAuth } from "@auth/sveltekit" 
+<!-- import { SvelteKitAuth } from "@auth/sveltekit" 
 import {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} from "courseRaterMidyear\my-app\.env" 
 import Google from "@auth/core/providers/google"; 
 
@@ -10,12 +10,12 @@ export const handle = SvelteKitAuth({
     import {signIn, signOut} from "@auth/sveltekit/client"
     import {page} from "$app/stores";
   </script>
-  
+   -->
 
   <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>
-    {#if $page.data.session}
+    <!-- {#if $page.data.session}
         <span>
             <small>Signed in as</small><br/>
             <strong>{$page.data.session.user?.name ?? "User"}</strong>
@@ -26,5 +26,5 @@ export const handle = SvelteKitAuth({
         <button on:click={() => signIn("google")}>
             Sign In with Google
         </button>
-    {/if}
+    {/if} -->
 </p>
